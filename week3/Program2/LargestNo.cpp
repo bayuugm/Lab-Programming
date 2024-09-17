5586c8bd65a35f0651106a9619f2f975e6816839
bayuugm <bayuputraibana@mail.ugm.ac.id>
2024-09-17 14:11:49 +0700

    Update LargestNo.cpp

--- week3/Program2/LargestNo.cpp
+++ week3/Program2/LargestNo.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-using namespace std;
+using namespace std; //to avoid repeating the std::
 
-int getInput(string prompt){
+int getInput(string prompt){ //get the input from the user
     int value;
     cout << prompt;
     cin >> value;
     return value;
 }
 
-void displayResult(int a, int b, int c, int largest){
-    if (a >= b && a >= c) {
+void displayResult(int a, int b, int c, int largest){ //function to display the results
+    if (a >= b && a >= c) { //check if a is the largest, if true then set largest = a
         largest = a;
-    }else if(b >= a && b >= c){
+    }else if(b >= a && b >= c){ //check if b is the largest, if true then set largest = b
         largest = b;
-    }else {
+    }else { //check if c is the largest, if true then set largest = c
         largest = c;
     }
-    cout << "The largest number is = " << largest << endl;
+    cout << "The largest number is = " << largest << endl; //print the largest number in the output after checking
 }
 
-int main(){
+int main(){ //asking the user for input and declaring the variables a,b,c as integers.
     int a = getInput("Enter the value of x= ");
     int b = getInput("Enter the value of y= ");
     int c = getInput("Enter the value of z= ");
