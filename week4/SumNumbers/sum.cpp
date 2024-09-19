9bb999a98d2bb6a57ec368e031fe3f14df92390b
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-09-19 23:39:50 +0700

    Add files via upload

---
+++ week4/SumNumbers/sum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,sum;
+    sum = 0;
+
+    cout << "Enter a Natural Number: ";
+    cin >> n;
+
+    if(n <= 0){
+        cout << "error: please enter a positive integer!" << endl;
+    }
+    else{
+        for (int i = 1; i <= n; ++i) {
+            sum += i;
+            if(i == 1){
+                cout << i;
+            }else{
+                cout << "+" << i;
+            }
+            
+        }
+        cout << " = " << sum << endl;
+
+        cout << "The Sum: " << sum << endl;
+        
+    }
+
+return 0;
+
+}
