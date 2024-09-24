5a253614edf622b0383962d5425be664f30ca2ec
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-09-25 06:49:35 +0700

    Update fizzbuzzTest.cpp

--- week5/Problem1/fizzbuzzTest.cpp
+++ week5/Problem1/fizzbuzzTest.cpp
@@ -6,40 +6,45 @@ using namespace std;
 int main() {
 
     vector<string> result;
+    vector<string> expectedOutput = {
+        "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", 
+        "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", 
+        "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz", 
+        "31", "32", "Fizz", "34", "Buzz", "Fizz", "37", "38", "Fizz", "Buzz", 
+        "41", "Fizz", "43", "44", "FizzBuzz", "46", "47", "Fizz", "49", "Buzz", 
+        "Fizz", "52", "53", "Fizz", "Buzz", "56", "Fizz", "58", "59", "FizzBuzz", 
+        "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "Buzz", 
+        "71", "Fizz", "73", "74", "FizzBuzz", "76", "77", "Fizz", "79", "Buzz", 
+        "Fizz", "82", "83", "Fizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz", 
+        "91", "92", "Fizz", "94", "Buzz", "Fizz", "97", "98", "Fizz", "Buzz"
+    };
 
-    for(int i = 1;i<=100; i++){
-        if(i % 3 == 0 && i % 5 == 0)
-        {
+    for(int i = 1; i <= 100; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
             result.push_back("FizzBuzz");
-        }else if(i % 3 == 0)
-        {    
+        } else if(i % 3 == 0) {    
             result.push_back("Fizz");
-
-        }else if(i % 5 == 0)
-        {
+        } else if(i % 5 == 0) {
             result.push_back("Buzz");
-        }else
-        {
+        } else {
             result.push_back(to_string(i));
-            
         }
     }
 
+    
     for(const string& s : result) {
-            cout << s << " ";
+        cout << s << " ";
     }
     cout << endl;
-return 0;
-}
 
+    
+    bool testPassed = (result == expectedOutput);
 
-string expectedOutput = 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz 
+    if(testPassed) {
+        cout << "Test Successful" << endl;
+    } else {
+        cout << "Test Unsuccessful" << endl;
+    }
 
-bool testPassed = false;
-if(result == expectedOutput) testPassed = True;
-
-if(testPassed == True) {
-  cout << "Test Succesful" << endl;
-}else {
-  cout << "Test Unsuccesful" << endl;
+    return 0;
 }
