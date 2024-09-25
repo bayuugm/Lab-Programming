5e2af3ea2191228e7a883552c446fee0e83a18f5
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-09-26 00:40:58 +0700

    Update ReverseArrayTest.cpp

--- week_5/problem_2/ReverseArrayTest.cpp
+++ week_5/problem_2/ReverseArrayTest.cpp
@@ -2,34 +2,37 @@
 #include <vector>
 
 int main() {
-    int n = 5
+    int n = 5;  
 
-    std::vector<int> a(n) = {1, 2, 3, 4, 5,};
-    std::vector<int> expectedOutput = Array: 1 2 3 4 5
-Reversed Array: 5 4 3 2 1
+    std::vector<int> a = {1, 2, 3, 4, 5};  
+    std::vector<int> expectedArrayOutput = {1, 2, 3, 4, 5};
+    std::vector<int> expectedReverseOutput = {5, 4, 3, 2, 1};
 
     std::cout << "Array: ";
-    for (int i = 0; i <n; i++) {
+    std::vector<int> resultArray;
+    for (int i = 0; i < n; i++) {
         std::cout << a[i] << " ";
+        Array.push_back(a[i]);  
     }
 
     std::cout << std::endl;
 
     std::cout << "Reversed Array: ";
-    for (int i = n-1; i >= 0; i--){
+    std::vector<int> resultReverseArray;
+    for (int i = n - 1; i >= 0; i--) {
         std::cout << a[i] << " ";
+        resultReverseArray.push_back(a[i]);  
     }
 
     std::cout << std::endl;
 
-    bool testPassed = (result == expectedOutput);
+    bool testPassed = (resultArray == expectedArrayOutput && resultReverseArray == expectedReverseOutput);
 
     if (testPassed) {
         std::cout << "TEST SUCCESSFUL!" << std::endl;
     } else {
         std::cout << "ERROR: TEST UNSUCCESSFUL!" << std::endl;
-        }
-        std::cout << std::endl;
-    
+    }
+
     return 0;
 }
