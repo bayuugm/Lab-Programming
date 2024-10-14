766d3fe597d5701f8dee415e52584b65bda752fb
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-10-14 14:56:17 +0700

    Update student_dataTest.cpp

--- week_6/problem_1/student_dataTest.cpp
+++ week_6/problem_1/student_dataTest.cpp
@@ -2,44 +2,57 @@
 using namespace std;
 
 struct students {
-    float uts;
-    float uas;
-    float average;
-    int nim;
+	float uts;
+	float uas;
+	float average;
+	int nim;
 };
 
-int main(){
-    int N = 4;
+int main() {
+	int N = 4;
 
-    struct students data[50];
-  data[0].nim = 446;
-  data[0].uts = 90;
-  data[0].uas = 95;
-  data[1].nim = 675;
-  data[1].uts = 67.8;
-  data[1].uas = 71.1;
-  data[2].nim = 253;
-  data[2].uts = 100;
-  data[2].uas = 50;
-  data[3].nim = 781;
-  data[3].uts = 90.85;
-  data[3].uas = 45.9;
+	struct students data[N];
+	data[0].nim = 446;
+	data[0].uts = 90;
+	data[0].uas = 95;
+	data[1].nim = 675;
+	data[1].uts = 67.8;
+	data[1].uas = 71.1;
+	data[2].nim = 253;
+	data[2].uts = 100;
+	data[2].uas = 50;
+	data[3].nim = 781;
+	data[3].uts = 90.85;
+	data[3].uas = 45.9;
 
 
-    for(int i = 0; i < N;i++){
-        data[i].average = (data[i].uts + data[i].uas) / 2;
-    }
+	for(int i = 0; i < N; i++) {
+		data[i].average = (data[i].uts + data[i].uas) / 2;
+	}
 
-    cout << "LIST OF STUDENT SCORES" << endl;
-    for(int i = 0; i < N;i++){
-        cout << "------------" << endl;
-        cout << "NIM: " << data[i].nim << endl;
-        cout << "AVERAGE: " << data[i].average << endl;
-    }
+	struct student expectedData[N];
+	expectedData[0].nim = 446;
+	expectedData[0].uts = 90;
+	expectedData[0].uas = 95;
+	expectedData[0].average = 92.5;
+	expectedData[1].nim = 675;
+	expectedData[1].uts = 67.8;
+	expectedData[1].uas = 71.1;
+	expectedData[1].average = 69.45;
+	expectedData[2].nim = 253;
+	expectedData[2].uts = 100;
+	expectedData[2].uas = 50;
+	expectedData[2].average = 75;
+	expectedData[3].nim = 781;
+	expectedData[3].uts = 90.85;
+	expectedData[3].uas = 45.9;
+	expectedData[3].average = 68.375;
 
-    bool testPassed = True 
+	if(
+    if(testPassed){
+        
 
-    
-    return 0;
-    
+
+return 0;
+
 }
