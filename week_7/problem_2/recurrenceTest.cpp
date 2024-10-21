8cfb8c8f58413a58138232d312e7f9b36ac720e1
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-10-21 16:50:05 +0700

    Update recurrenceTest.cpp

--- week_7/problem_2/recurrenceTest.cpp
+++ week_7/problem_2/recurrenceTest.cpp
@@ -13,21 +13,20 @@ int A(int n){
 int main(){
     int num = 7;
     int expectedResult = 5040;
-
-    while (!(cin >> num) || num <= 0) {
-        cout << "Invalid input! Please enter a natural number: ";
-    }
+    
     int result = A(num);
 
     cout << "Factorial of " << num << " = " << result << endl;
 
-    bool testPassed = result == expectedResult;
+    bool testPassed = (result == expectedResult);
 
     if (testPassed) {
-      cout << "TEST SUCCESSFUL" << endl;
-        else {
-        cout << "TEST UNSUCCESSFUL" << endl;
+        std::cout << "TEST SUCCESSFUL!" << std::endl;
+    } else {
+        std::cout << "ERROR: TEST UNSUCCESSFUL!" << std::endl;
         }
-    }
+        std::cout << std::endl;
+
+    return 0;
     
 }
