b33a7a333897442bb62e9fc21a2356879963a968
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-10-21 16:47:19 +0700

    Create recurrenceTest.cpp

---
+++ week_7/problem_2/recurrenceTest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int A(int n){
+    
+    if (n <= 1){
+        return 1;
+    }
+
+    return n * A(n - 1);
+}
+
+int main(){
+    int num = 7;
+    int expectedResult = 5040;
+
+    while (!(cin >> num) || num <= 0) {
+        cout << "Invalid input! Please enter a natural number: ";
+    }
+    int result = A(num);
+
+    cout << "Factorial of " << num << " = " << result << endl;
+
+    bool testPassed = true;
+
+    if (testPassed) {
+      cout 
+    
+}
