27a2706bfc2ea157bc7b47b7e4399fa2fd6b1a4a
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-11-02 22:15:11 +0700

    Update sortingmethodTest.cpp

--- week_8/Problem_1/sortingmethodTest.cpp
+++ week_8/Problem_1/sortingmethodTest.cpp
@@ -10,25 +10,22 @@ struct Students {
 
 void display(Students data[], int size) {
     for (int i = 0; i < size; ++i) {
-        cout << "NISN:" << data[i].NISN << " ,Nama:" << data[i].Nama << " ,Value:" << data[i].Value << endl;
+        cout << "NISN: " << data[i].NISN << " ,Nama: " << data[i].Nama << " ,Value: " << data[i].Value << endl;
     }
     cout << endl;
 }
 
 void insertionSortNISN(Students data[], int size){
-    for(int i=1;i < size; i++){
-    Students temp = data[i];
-    int j = i - 1;
-    while(j >= 0 && data[j].NISN < temp.NISN){
-        data[j+1] = data[j];
-        j--;
+    for(int i = 1; i < size; i++){
+        Students temp = data[i];
+        int j = i - 1;
+        while(j >= 0 && data[j].NISN > temp.NISN){
+            data[j + 1] = data[j];
+            j--;
+        }
+        data[j + 1] = temp;
     }
-    data[j+1] = temp;
-    }
-    display(data,size);
 }
-
-
 
 int main(){
     Students data[] = {
@@ -42,14 +39,27 @@ int main(){
     };
     int size = sizeof(data) / sizeof(data[0]);
 
+    insertionSortNISN(data, size);
+
     Students expectedData[] = {
-        {9970293945, "Alivia Rahma Pramesti", 70},
-        {9970272750, "Achmad Yaumil Fadjri R.", 60},
-        {9965653989, "Arief Budiman", 60},
-        {9963959682, "Rio Alfandra", 55},
-        {9960312699, "Handi Ramadhan", 90},
-        {9952382180, "Ari Lutfianto", 65},
         {9950310962, "Ronaldo Valentino Uneputty", 80},
+        {9952382180, "Ari Lutfianto", 65},
+        {9960312699, "Handi Ramadhan", 90},
+        {9963959682, "Rio Alfandra", 55},
+        {9965653989, "Arief Budiman", 60},
+        {9970272750, "Achmad Yaumil Fadjri R.", 60},
+        {9970293945, "Alivia Rahma Pramesti", 70},
+    };
+
+    bool testPassed = true;
+    for (int i = 0; i < size; i++) {
+        if (data[i].NISN != expectedData[i].NISN 
+        || data[i].Nama != expectedData[i].Nama 
+        || data[i].Value != expectedData[i].Value) {
+            testPassed = false;
+            break;
+        }
+    }
 
     if (testPassed) {
         std::cout << "TEST SUCCESSFUL!" << std::endl;
@@ -57,6 +67,5 @@ int main(){
         std::cout << "ERROR: TEST UNSUCCESSFUL!" << std::endl;
     }
 
-
-
+    return 0;
 }
