015cecd29dc8db95336ad95d8d70673764732e92
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-11-24 17:09:15 +0700

    Create areacircleTest.cpp

---
+++ responsi/p1/areacircleTest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double area; 
+  float radius = 8;
+  float pi = 3.14;
+  float expectedResult = 
+
+  cout << "Enter the radius of the circle: " << endl;
+  cin >> radius;
+
+  area = pi * (radius*radius);
+  cout << "Area of the circle: " << area << endl;
+  
+
+  if(expectedResult == area){
+    cout << "TEST SUCCESSFUL! << endl;
+      }else{
+    cout << "TEST UNSUCCESSFUL! << endl;
+    }
+
+  return 0;
+}
