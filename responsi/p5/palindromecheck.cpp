36ec3583267ef4a89e4757830ac137907af994cb
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-11-24 20:25:13 +0700

    Update palindromecheck.cpp

--- responsi/p5/palindromecheck.cpp
+++ responsi/p5/palindromecheck.cpp
@@ -8,9 +8,16 @@ int main(){
   cin >> word;
 
   for(int i = word.length() - 1; i >= 0; i--){
-    
+    reversed += word[i];
   }
 
+  //check
+  if(reversed == word){
+    cout << word << " is a palindrome." << endl;
+  }else{
+    cout << word << " is not a palindrome." << endl;
+  }
+  
   
 }
 
